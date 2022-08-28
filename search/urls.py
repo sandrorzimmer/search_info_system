436a4950ec7e1f754dc7c6fdcabf4522a0289494
Sandro Zimmer <sandrorzimmer@gmail.com>
2022-08-28 15:53:15 -0300

    Changes to test deplyment in Pythonanywhere.

--- search/urls.py
+++ search/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('search_info/', include('search_info.urls')),
+#    path('search_info/', include('search_info.urls')),
+    path('', include('search_info.urls')),
     path('admin/', admin.site.urls),
 ]
--- search_info/urls.py
+++ search_info/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path("", views.index, name="index"),
 
     # API Routes
-    path("new_tag", views.new_tag, name="new_tag"),
-    path("new_info", views.new_info, name="new_info"),
-    path("search_info", views.search_info, name="search_info"),
-    path("edit_info", views.edit_info, name="edit_info"),
-    path("delete_info", views.delete_info, name="delete_info"),
-    path("edit_tag", views.edit_tag, name="edit_tag"),
-    path("delete_tag", views.delete_tag, name="delete_tag")
+    path("search_info/new_tag", views.new_tag, name="new_tag"),
+    path("search_info/new_info", views.new_info, name="new_info"),
+    path("search_info/search_info", views.search_info, name="search_info"),
+    path("search_info/edit_info", views.edit_info, name="edit_info"),
+    path("search_info/delete_info", views.delete_info, name="delete_info"),
+    path("search_info/edit_tag", views.edit_tag, name="edit_tag"),
+    path("search_info/delete_tag", views.delete_tag, name="delete_tag")
 ]
